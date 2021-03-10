1276a1dba785253e82aaf993a32bd91894a3fc3c
Mike <michaelw1025@gmail.com>
2021-03-10 06:10:27 -0600

    styles

--- lebwebstyles/tailwind.config.js
+++ lebwebstyles/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   future: {
       removeDeprecatedGapUtilities: true,
@@ -12,6 +14,23 @@ module.exports = {
   },
   theme: {
       extend: {},
+  },
+  colors: {
+    white: colors.white,
+    black: colors.black,
+    gray: colors.blueGray,
+    red: colors.red,
+    orange: colors.orange,
+    yellow: colors.yellow,
+    green: colors.green,
+    emerald: colors.emerald,
+    teal: colors.teal,
+    cyan: colors.cyan,
+    lightBlue: colors.lightBlue,
+    blue: colors.blue,
+    purple: colors.purple,
+    pink: colors.pink,
+    rose: colors.rose,
   },
   variants: {},
   plugins: [],
